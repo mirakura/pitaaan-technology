e9a63c7bc1134df5ff6c0c7ede438c18b5433489
take1-MacA1502 <take1.kouji@gmail.com>
2025-11-17 21:50:51 +0900

    change/top:headerとfooterについて、レンレンさんの指示の通り

--- script.js
+++ script.js
@@ -45,38 +45,3 @@ document.addEventListener("DOMContentLoaded", function () {
     isAutoplayPlaying = !isAutoplayPlaying;
   });
 });
-
-// ===========================
-// 🚚 ヘッダー・フッター読み込み
-// ===========================
-
-document.addEventListener("DOMContentLoaded", () => {
-  // ヘッダーを読み込み、挿入する要素を指定
-  fetch("components/header/index.html")
-    .then(response => response.text()) // レスポンスをテキスト形式で取得
-    .then(data => {
-      document.getElementById("header-placeholder").innerHTML = data; // HTML要素に挿入
-    })
-    .catch(error => console.error("Error loading header:", error));
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  // フッターを読み込み、挿入する要素を指定
-  fetch("components/footer/index.html")
-    .then(response => response.text()) // レスポンスをテキスト形式で取得
-    .then(data => {
-      document.getElementById("footer-placeholder").innerHTML = data; // HTML要素に挿入
-    })
-    .catch(error => console.error("Error loading header:", error));
-});
-
-// ===========================
-// 🍔 ハンバーガーメニュー開閉制御
-// ===========================
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navMenu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
